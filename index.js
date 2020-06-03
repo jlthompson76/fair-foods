00a06248ef01b74f392103a6c5b6013843e31940
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-06-03 00:46:49 -0400

    Modify index.js file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const body_parser = require('body-parser');
 const mongodb = require('mongodb');
 const dotenv = require('dotenv').config();
 const path = require('path');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -18,20 +19,18 @@ const DB_URL = process.env.DB_URL;
 const DB_NAME = process.env.DB_NAME;
 const COLLECTION_NAME = process.env.COLLECTION_NAME;
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use('/', routes);
 
 app.listen(PORT, () => {
-    console.log(`Fair Foods is listening on port ${PORT}!`);
+  console.log(`Fair Foods is listening on port ${PORT}!`);
 
-    let mongo_client = mongodb.MongoClient;
-    mongo_client.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db_client) => {
-        if (err) {
-            console.log(`Error: ${err}`);
-        } else {
-            console.log('Database Connected');
-            db_handler = db_client.db(DB_NAME);
-        }
-    });
+  let mongo_client = mongodb.MongoClient;
+  mongo_client.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db_client) => {
+    if (err) {
+      console.log(`Error: ${err}`);
+    } else {
+      console.log('Database Connected');
+      db_handler = db_client.db(DB_NAME);
+    }
+  });
 });
